90053f676470ccd33cdb71fa06f0e27c18ab611a
Juan Gabito <gabitoju@gmail.com>
2025-06-23 16:13:39 -0300

    Modifications

--- init.lua
+++ init.lua
@@ -1,23 +1,30 @@
-local home = os.getenv('HOME')
+local home = os.getenv("HOME")
 
 require("extra")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
-
 
 require("vim-options")
 require("lazy").setup("plugins")
 
-local theme = require('last-color').recall() or 'nord'
-vim.cmd(('colorscheme %s'):format(theme))
+local theme = require("last-color").recall() or "nord"
+vim.cmd(("colorscheme %s"):format(theme))
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.textwidth = 120
+        vim.opt_local.formatoptions:append("t")
+    end,
+})
--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,6 +1,10 @@
 return {
     "m4xshen/autoclose.nvim",
     config = function ()
-        require("autoclose").setup({})
+        require("autoclose").setup({
+            options = {
+                disabled_filetypes = {"text", "markdown"},
+            }
+        })
     end
 }
